5cdb8efde54e8dc76a871f4b710b936bf2064d1a
josekim <joseph.kim85@gmail.com>
2017-05-10 13:21:17 -0500

    Add prompt to hamster.rb

---
+++ ruby/hamsters.rb
@@ -0,0 +1,11 @@
+
+puts "What is the hamster's name?"
+  name = gets.chomp
+puts "How loud is the hamster? (1-10)"
+  volume = gets.chomp
+puts "What color fur does it have?"
+  fur= gets.chomp
+puts "Is this a good candidate for adoption?"
+  verdict = gets.chomp
+puts "Estimated age of hamster?"
+  age = gets.chomp
