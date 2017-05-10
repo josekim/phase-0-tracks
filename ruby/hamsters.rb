95a155b8487ecce7b9b5550408cdf4da9d6fbc1d
josekim <joseph.kim85@gmail.com>
2017-05-10 13:59:31 -0500

    add print console

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,11 +1,33 @@
 
-puts "What is the hamster's name?"
+puts "What is the hamsters name?"
   name = gets.chomp
 puts "How loud is the hamster? (1-10)"
-  volume = gets.chomp
+  volume = gets.chomp.to_i
 puts "What color fur does it have?"
-  fur= gets.chomp
-puts "Is this a good candidate for adoption?"
-  verdict = gets.chomp
-puts "Estimated age of hamster?"
+  fur = gets.chomp
+verdict = nil
+until verdict != nil
+puts "Is this a good candidate for adoption (y/n)?"
+  verdict = gets.chomp.downcase
+  if verdict == "y"
+    verdict = true
+  elsif verdict == "n"
+    verdict = false
+  else
+    verdict = nil
+  end
+end
+
+puts "What is the hamster's age?"
   age = gets.chomp
+if age == ""
+  age = nil
+else
+  age.to_i
+end
+
+puts "Hamster:    #{name}."
+puts "Loudness:   #{volume}."
+puts "Fur Color:  #{fur}."
+puts "Adoption:   #{verdict}"
+puts "Hamster Age:#{age}"
