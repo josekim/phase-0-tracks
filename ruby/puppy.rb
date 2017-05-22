417778f27574736619ca0f56f8c477219422f3b2
josekim <joseph.kim85@gmail.com>
2017-05-22 12:00:36 -0500

    add Class Pseudo code for Puppy Class

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+#Puppy Description
+#Species-------------
+# Puppy Ficticious
+
+# Characteristics --------
+# Alert : Yes
+# Sense of smell: yes
+# Playfulness : yes
+# fluffliness : ITS SO FLUFFY!
+# Name : Varies
+# gender : varies
+
+# Behavior
+# Guard
+# Smell
+# Fluffly_Attack
+# Bark
