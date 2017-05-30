a72d3d16b948d67fab655c871a02362db01b95f8
josekim <joseph.kim85@gmail.com>
2017-05-30 13:22:13 -0500

    Add Reverse Function for Words in JS

---
+++ js/explore.js
@@ -0,0 +1,30 @@
+/*
+Pseudo Code - Reverse
+
+Take new word or phrase
+starting at the first letter
+  put letter at the front of the array.
+When Finish return new word
+
+
+*/
+
+
+function reverseWord(word)  {
+  word = word.split("")
+  let length = word.length;
+  let newWord = []
+  for (var i = 0; i < length; i++ ){
+    newWord.unshift(word[i])
+  }
+  return newWord.join("");
+}
+
+/*****************Driver********************/
+let driverWord = "Hello"
+let newDriverWord = reverseWord(driverWord)
+
+if (driverWord != newDriverWord){
+  console.log(newDriverWord)
+  console.log(driverWord)
+}
