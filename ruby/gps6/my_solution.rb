56c9cef0eca388080dea2456b6c6e18d91bdf80b
josekim <joseph.kim85@gmail.com>
2017-05-31 18:19:21 -0500

    add reflection and refactoring

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -18,46 +18,44 @@ class VirusPredictor
 
   # virus_effect - Running predicted_deaths and speed_of_spread - Public method
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
   # Predicts death as percentage of population at different population density thresholds
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      rate = 0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      rate = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      rate = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      rate = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      rate = 0.05
     end
-
+    number_of_deaths = (@population * rate).floor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
   # Predicts the number of months for disease to spread using population density
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
-
     if @population_density >= 200
-      speed += 0.5
+      speed = 0.5
     elsif @population_density >= 150
-      speed += 1
+      speed = 1
     elsif @population_density >= 100
-      speed += 1.5
+      speed = 1.5
     elsif @population_density >= 50
-      speed += 2
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
@@ -90,5 +88,26 @@ STATE_DATA.each do |state, population_hash|
   new_state.virus_effects
 end
 
-#=======================================================================
+#======================================================================
+=begin
 # Reflection Section
+What are the differences between the two different hash syntaxes shown in the state_data file?
+  One uses the actual string as the key, The other uses a symbol.  Its helpful since the string is more presentable and more easilty used to present the data
+
+What does require_relative do? How is it different from require?
+  Require's a file that is in the relative path, as in the same directory.  Require is able to specify where its being require from, like absolute path, or through library or gem
+
+What are some ways to iterate through a hash?
+  Use the each method, each_key,
+
+When refactoring virus_effects, what stood out to you about the variables, if anything?
+  Sometimes you know what is being repeated, but not quite sure how to get rid of what is being Repeated.
+
+What concept did you most solidify in this challenge?
+  I think i'm okay with most concetps, need to work on Refactoring.
+
+
+
+
+
+=end
