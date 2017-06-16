b938a0217b0857f4a4fb7654c593414ad2da3820
josekim <joseph.kim85@gmail.com>
2017-06-15 20:54:39 -0500

    add release 0 and 1 code

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,6 +17,11 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/new' do
+  @students = db.execute("SELECT * FROM students")
+  erb :new
+end
+
 # create new students via
 # a form
 post '/students' do
